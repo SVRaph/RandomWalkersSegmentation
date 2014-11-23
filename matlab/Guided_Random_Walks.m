10395cc340f86e653e87c909e7d46da15753b43e
SvRaph <siverar@eleves.enpc.fr>
2014-11-23 20:24:24 +0100

    running guided random walks

--- matlab/Guided_Random_Walks.m
+++ matlab/Guided_Random_Walks.m
@@ -1,8 +1,104 @@
-function X_k = Guided_Random_Walks(I,R_k,B_k)
+function X_k = Guided_Random_Walks(I,R,B,seeds,alpha,beta,gamma)
 
-% Guided_Random_Walk - segmente l'image I en utilisant le driver (R_k,B_k) 
+%% TODO 
+% - commentaires
+% - correction erreurs présentes dans l'article
+% - efficaité
 
-% TODO
+% Guided_Random_Walk - segmente l'image I en utilisant le driver (R_k,B_k)
 
-X_k=B_k;
+sz=size(I);
+v_sub2ind=[1,sz(1),sz(1)*sz(2)];
+N=sz(1)*sz(2)*sz(3);
+
+disp(['Starting guided random walks ',int2str(N)]);
+
+% 6-voisinage
+sub_c_II= [1,0,0;-1,0,0;0,1,0;0,-1,0;0,0,1;0,0,-1]              + 2;
+c_II =    sub2ind(sz,sub_c_II(:,1),sub_c_II(:,2),sub_c_II(:,3)) - sub2ind(sz,2,2,2) ; % pdt scalR with v ?
+c_IR = [c_II;0]; %ind_c_IR=[c_II;0,0,0];
+
+    function [W,O]=weights_matrices(I,R,N,c_II,c_IR,alpha,beta)
+        % compute the W and the Omega matrices
+        W=spalloc(N,N,size(c_II,1)*N);
+        O=spalloc(N,N,size(c_IR,1)*N);
+        for i=1:N
+            if not(mod(i,100))
+                disp(i);
+            end
+            for l=1:size(c_II,1)
+                j=i+c_II(l);
+                if (j >= 1 && j <= N)
+                    W(i,j) = exp(-alpha*(I(i)-I(j)));
+                end
+            end
+            
+            for l=1:size(c_IR,1)
+                j=i+c_IR(l);
+                if (j >= 1 && j <= N)
+                    O(i,j) = exp(-beta*(I(i)-R(j)));
+                end
+            end
+        end
+    end
+
+    function [L,A]=energy_matrices(W,O)
+        L=-2*W;
+        A=spalloc(N,N,N);
+        
+        for i=1:N
+            if not(mod(i,100))
+                disp(i);
+            end
+            L(i,i)=2*sum(W(i,:));
+            A(i,i)=sum(O(i,:));
+        end
+    end
+
+
+% Matrices W, Omega, L, A
+disp('W');
+[W,O]=weights_matrices(I,R,N,c_II,c_IR,alpha,beta);
+disp('L,A');
+[L,A]=energy_matrices(W,O);
+disp('done');
+
+
+% Indices Marked et Unmarked
+indM1=reshape(seeds(1,:,:)-1,size(seeds,2),3)*v_sub2ind'+1;
+indM2=reshape(seeds(2,:,:)-1,size(seeds,2),3)*v_sub2ind'+1;
+indM=[indM1;indM2];
+
+logicalM=false(N,1);
+for i=1:size(indM,1)
+    logicalM(round(indM(i)))=true;
+end
+logicalU=not(logicalM);
+
+% Système sparse
+Lu=L(logicalU,logicalU);
+Lb=L(logicalM,logicalU);
+Au=A(logicalU,logicalU);
+Ab=A(logicalM,logicalU);
+Omegab=O(logicalM,logicalU);
+Omegau=O(logicalU,logicalU);
+
+bm=B(logicalM);
+bu=B(logicalU);
+xm=[zeros(size(indM1,1),1);ones(size(indM1,1),1)];
+
+
+MA = Lu+gamma*Au;
+Mb = gamma*Omegab'*bm + gamma*Omegau*bu - Lb'*xm + gamma*Ab'*xm;
+
+disp('Sparse linear system created\n');
+
+% solve MA*x=Mb
+xu=pcg(MA,Mb);
+
+X_k=zeros(size(B));
+X_k(logicalU)=xu;
+
+disp('Sparse linear system solved\n');
+
 end
--- matlab/main.m
+++ matlab/main.m
@@ -15,15 +15,19 @@
 %% Paramètres
 Dice_threshold=0.5;
 
+alpha = 90 /100;
+beta  = 130/100;
+gamma = 0.4;
+
 
 %% Lecture des données
-sz = [100 100 100];
-c = [50 50 30];
+sz = [100 100 100]/5;
+c = [50 50 30]/5;
 ax1 = [1 1 0];
 ax2 = [-1 1 0];
 ax3 = [0 0 1];
 axes = [X1/sqrt(sum(X1.*X1)) ; X2/sqrt(sum(X2.*X2)) ; X3/sqrt(sum(X3.*X3))];
-rays = [40 20 5];
+rays = [40 20 5]/5;
 
 [R,B,~] = generate_heart(sz, c, axes, rays,0.3,Nseeds,3);
 
@@ -51,7 +55,7 @@ if 0
     %roi=min(512,max(1,[xg-dx,xg+dx,yg-dy,yg+dy]));
     roi=[xg-dx,xg+dx,yg-dy,yg+dy];
     if (any(roi<1) || any(roi>512))
-        mprintf('Your argument is invalid\n');
+        disp('Your argument is invalid\n');
         return;
     end
 
@@ -64,9 +68,9 @@ I=I0;
 
 %% Main loop on drivers
 for k=1:size(R,1)
-    R_k=R(k,:,:,:);
-    B_k=B(k,:,:,:);
-    X_k=Guided_Random_Walks(I,R_k,B_k);
+    R_k=squeeze(R(k,:,:,:));
+    B_k=squeeze(B(k,:,:,:));
+    X_k=Guided_Random_Walks(I,R_k,B_k,seeds_k,alpha,beta,gamma);
     D_k=Dice(X_k,B_k);
     
     if D_k>D_max
@@ -78,10 +82,9 @@ for k=1:size(R,1)
 end
     
 if D_max>Dice_threshold
-    mprintf('Best segmentation with driver '+int2str(indx)+'\nDice metrix='+num2str(D_max)+'\n')
-    imshow(Xopt);
+    disp('Best segmentation with driver '+int2str(indx)+'\nDice metrix='+num2str(D_max)+'\n')
+    %implay(Xopt);
 else
-    mprintf('No matching subject found\nPerforming conventional Random Walks\n');
-    Xopt=Random_Walks(I);  
+    disp('No matching subject found\nPerforming conventional Random Walks\n');
+    %Xopt=Random_Walks(I);  
 end
-imshow(Xopt);
