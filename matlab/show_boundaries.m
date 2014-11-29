2ef4d701e91fc6faa483dcec61d8ce218fb8027b
mrozand <m.rozand@gmail.com>
2014-11-29 17:09:15 +0100

    add function to show the image with theoretical and computed boundaries of the segmentation

---
+++ matlab/show_boundaries.m
@@ -0,0 +1,30 @@
+function [ colorIm ] = show_boundaries (I0,B0,Xopt,seg_threshold)
+
+% show the image with the theoretical and computed segmentation boundaries
+
+Bk=Xopt>0.5;
+[s1,s2,s3] = size(I0);
+colorIm = zeros(s1,s2,3,s3);
+
+% Compute the theoretical boundaries
+dif1 = B0-circshift(B0,[1,1]);
+dif2 = B0-circshift(B0,[1,2]);
+notTheoBound = (abs(dif1)+abs(dif2)==0);
+
+% Compute the computed boundaries
+dif1 = Bk-circshift(Bk,[1,1]);
+dif2 = Bk-circshift(Bk,[1,2]);
+notCompBound = (abs(dif1)+abs(dif2)==0);
+
+% Copy the image and boundaries into colored images space
+for k=1:s3
+    theoBound = ones(s1,s2)-notTheoBound(:,:,k);
+    compBound = ones(s1,s2)-notCompBound(:,:,k);
+    colorIm(:,:,:,k) = repmat( I0(:,:,k) .* notTheoBound(:,:,k) .* notCompBound(:,:,k) , [1,1,3])...
+        + cat(3 , theoBound.*notCompBound(:,:,k) , zeros(s1,s2) , zeros(s1,s2))...
+        + cat(3 , zeros(s1,s2) , compBound , zeros(s1,s2));
+end
+
+implay(colorIm);
+
+end
